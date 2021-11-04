c3be78a2da9c42443bb5c07a708fe4cf49c66b7c
Zanella Matteo <79336755+Zanzibarr@users.noreply.github.com>
2021-11-04 16:03:51 +0100

    Create Sample.cpp
    An example of VeneCode in action

---
+++ Sample.cpp
@@ -0,0 +1,12 @@
+#include "VeneCode.hpp"
+
+usaghe piassa san_marco;
+
+intiero fondamentae(vodo) {
+  digo << "Ghesboro fioi." << va_capo;
+  bastemia input;
+  dime >> input;
+  se (input.lunghessa xe_manco_de 3) digo << "Cojon." << va_capo;
+  altrimenti digo << "Bravo toso." << va_capo;
+  ridame 0;
+}
