4ed79f9bef03d8164abc6f119bfd1bada3c7c8d8
Zanella Matteo <79336755+Zanzibarr@users.noreply.github.com>
2021-11-04 15:57:10 +0100

    V0.0.1 (Pre-Alpha)

---
+++ VeneCode.hpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#pragma region Namespaces
+    #define piassa namespace
+    #define san_marco std
+#pragma endregion Namespaces
+
+#pragma region KeyWords
+    #define fermate break
+    #define situasion case
+    #define ciapa catch
+    #define fameja class
+    #define a_casso default
+    #define feto do
+    #define seno else
+    #define vien_fora exit
+    #define par_ogni for
+    #define se if
+    #define novo new
+    #define cassi_miei private
+    #define cassi_nostri protected
+    #define cassi_de_tutti public
+    #define ridame return
+    #define impianta static
+    #define coso struct
+    #define decidate switch
+    #define sto this
+    #define provaghe try
+    #define usaghe using
+    #define intanto_che while
+#pragma endregion
+
+usaghe piassa san_marco;
+
+#pragma region Types
+    #define condision bool
+    #define letera char
+    #define testardo const
+    #define virgoeona double
+    #define virgoetta float
+    #define intiero int
+    #define intiero_exagerato long
+    #define vodo void
+#pragma endregion
+
+#pragma region Types_Values
+    #define vero true
+    #define cassada false
+    #define na_tega nullptr
+#pragma endregion
+
+#pragma region Logic_Operators
+    #define xe ==
+    #define no_xe !=
+    #define xe_al_manco >=
+    #define xe_al_piu <=
+    #define xe_manco_de <
+    #define xe_piu_de >
+#pragma endregion
+
+#pragma region Arithmetic_Operators
+    #define piu +
+    #define manco -
+    #define par *
+    #define su /
+    #define che_xe =
+    #define daghene +=
+    #define cavaghe -=
+    #define daghene_uno ++
+    #define cavaghe_uno --
+#pragma endregion
+
+#pragma region Others
+    #define fondamentae main
+#pragma region
+
+#pragma region std
+    #define dime cin
+    #define digo cout
+    #define va_capo endl
+    #define grandessa_de sizeof
+    #define dame_intero stoi
+#pragma endregion
+
+#pragma region string
+    #define bastemia string
+    #define lunghessa length()
+    #define scurta substr
+#pragma endregion
+
+#pragma region vector
+    #define trenin vector
+    #define butaghe push_back
+    #define grandessa size()
+#pragma endregion
